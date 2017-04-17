8125d042675ad68138c465fdc0cc58bacf00f9d9
Luca Sgroi <lucasgroi@gmail.com>
2017-04-17 12:00:26 +0100

    Update dependencies

--- .build/tasks/Test-PackageInstallation.ps1
+++ .build/tasks/Test-PackageInstallation.ps1
@@ -4,10 +4,10 @@ Import-Script Pask.Tests.Infrastructure -Package Pask
 Task Test-PackageInstallation Clean, Pack-Nuspec, Push-Local, {
     $Assertion = {
         $PaskTestVersion = (([xml](Get-Content (Join-Path $ProjectFullPath "Pask.Test.dotCover.nuspec"))).package.metadata.dependencies.dependency | Where { $_.id -eq "Pask.Test" }).version
-        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application.Domain\packages.config"))).packages.package | Where { $_.id -eq "Pask.Test" }).version -eq $PaskTestVersion) "Incorrect version of Pask.Test installed into project 'Application.Domain'"
+        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application\packages.config"))).packages.package | Where { $_.id -eq "Pask.Test" }).version -eq $PaskTestVersion) "Incorrect version of Pask.Test installed into project 'Application'"
         $dotCoverVersion = (([xml](Get-Content (Join-Path $ProjectFullPath "Pask.Test.dotCover.nuspec"))).package.metadata.dependencies.dependency | Where { $_.id -eq "JetBrains.dotCover.CommandLineTools" }).version
-        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application.Domain\packages.config"))).packages.package | Where { $_.id -eq "JetBrains.dotCover.CommandLineTools" }).version -eq $dotCoverVersion) "Incorrect version of JetBrains.dotCover.CommandLineTools installed into project 'Application.Domain'"
+        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application\packages.config"))).packages.package | Where { $_.id -eq "JetBrains.dotCover.CommandLineTools" }).version -eq $dotCoverVersion) "Incorrect version of JetBrains.dotCover.CommandLineTools installed into project 'Application'"
     }
 
-    Test-PackageInstallation -Name Pask.Test.dotCover -Assertion $Assertion -InstallationTargetInfo "Install into 'Application.Domain' project"
+    Test-PackageInstallation -Name Pask.Test.dotCover -Assertion $Assertion -InstallationTargetInfo "Install into 'Application' project"
 }
